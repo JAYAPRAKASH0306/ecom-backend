fbcb1b79b2a5d6d27bb4e53720c686c7f37d3ef7
JAYAPRAKASH0306 <jayaprakashk0306@gmail.com>
2023-12-03 23:31:04 +0530

    second

--- products.js
+++ products.js
@@ -1,30 +1,27 @@
 const products = [
   {
     id: 1,
-    name: "iPhone 12 Pro",
+    name: "iPhone 15 Pro",
     brand: "Apple",
     desc: "6.1-inch display",
     price: 999,
-    image:
-      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/iphone12pro_e09phn.jpg",
+    image:"https://th.bing.com/th/id/OIP.F-C88NI73rbina9jX26YwwHaHa?w=194&h=194&c=7&r=0&o=5&dpr=1.5&pid=1.7"
   },
   {
     id: 2,
-    name: "iPhone 12",
-    brand: "Apple",
+    name: "samsung fold",
+    brand: "samsung",
     desc: "5.4-inch mini display",
     price: 699,
-    image:
-      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/iphone12_efhrcp.jpg",
+    image:"https://th.bing.com/th/id/OIP.RuSXA7qzyB7ImZOrVEPM1gHaHa?w=154&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"
   },
   {
     id: 3,
-    name: "Galaxy S",
+    name: "Galaxy flip",
     brand: "Samsung",
     desc: "6.5-inch display",
     price: 399,
-    image:
-      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/galaxyS_dvjf5w.png",
+    image:"https://th.bing.com/th/id/OIP.arLmruUF4je8adIjc7jI1AHaHa?w=168&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"
   },
 ];
 
