64a4f43af76cd3e1854b121c30b824dd4a6c099d
JAYAPRAKASH0306 <jayaprakashk0306@gmail.com>
2023-12-04 10:16:46 +0530

    model commit

--- routes/orders.js
+++ routes/orders.js
@@ -10,7 +10,7 @@ const router = require("express").Router();
 
 router.post("/", auth, async (req, res) => {
   const newOrder = new Order(req.body);
-
+console.log(newOrder);
   try {
     const savedOrder = await newOrder.save();
     res.status(200).send(savedOrder);
